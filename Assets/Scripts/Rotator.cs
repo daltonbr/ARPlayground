3385ca1d576001a2985bd2ade1a41254537b9af8
Dalton <Lima>
2020-02-20 20:57:49 +0100

    Add select, rotate, scale, and translate

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private Vector3 speed;
+    
+    private void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime, Space.Self);
+    }
+}
