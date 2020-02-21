9e4171b033a27ed326924f9552b5886ec407b1ce
Dalton <Lima>
2020-02-21 16:20:23 +0100

    Add my custom package DaltonLima Core

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.PlayerLoop;
+
+//TODO: implement Singleton
+public class Logger : MonoBehaviour
+{
+   [SerializeField] private TextMeshPro debugAreaText;
+   [SerializeField] private bool enableDebug;
+   [SerializeField] private int maxLines;
+
+   private void OnEnable()
+   {
+      debugAreaText.enabled = enableDebug;
+      enabled = enableDebug;
+   }
+
+   private void OnDisable()
+   {
+      throw new NotImplementedException();
+   }
+
+   public void LogInfo(string message)
+   {
+      //ClearLines();
+      //debugAreaText.text += $"(DateTime.Now.ToString("HH-mm-ss"))color=\"white\">(message)</color>\n";
+   }
+
+   public void LogError()
+   {
+      
+   }
+
+}
